30a7962950a99649feb884bed65ed38c9f1ea6f1
rodrigobertini <rodrigobertini11@gmail.com>
2016-05-03 11:57:05 -0300

    Update ProductController.cs

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,4 +1,4 @@
-﻿using Domain2;
+using API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,52 +10,56 @@ namespace API.Controllers
 {
     public class ProductController : ApiController
     {
-        List<Product> listaProdutos = new List<Product>();
 
+        static List <Product> listaProdutos = new List <Product>();
+ 
+        
+        //public ProductController - metodo construtor
+        public ProductController() {
 
-        // GET: api/Product
+            // List<Product> listaProdutos = new List<Product>();
+            //variavel produto(x) recebe os parametros da classe Product
+            var produto1 = new Product { Id = 1, Name = "Yo- yo", Category = "Brinquedos", Price = 3.50m };
+            var produto2 = new Product { Id = 2, Name = "Placa mãe", Category = "Hardware", Price = 400m };
+            var produto3 = new Product { Id = 3, Name = "Chocolate", Category = "doces", Price = 4.40m };
 
-        public IHttpActionResult Get()
-        {
-            var produto1 = new Product();
-            produto1.ID = 1;
-            produto1.Name = "Placa de Rede";
-            produto1.Category = "Hardware";
-            produto1.Price = 120m;
+            listaProdutos.Add(produto1);
+            listaProdutos.Add(produto2);
+            listaProdutos.Add(produto3);
+           
+        }
 
-            var produto2 = new Product { ID = 2, Name = "Placa mãe", Category = "Hardware", Price = 480m };
-            var produto3 = new Product { ID = 3, Name = "Antivirus", Category = "Software", Price = 180m };
+        // GET: api/Product        
+        public IHttpActionResult Get() { 
+        
+            var produto1 = new Product{Id = 1, Name = "Yo- yo", Category = "Brinquedos", Price = 3.50m };
+            var produto2 = new Product{Id = 2, Name = "Placa mãe", Category = "Hardware", Price = 400m };
+            var produto3 = new Product{Id = 3, Name = "Chocolate", Category = "doces", Price = 4.40m };
 
             listaProdutos.Add(produto1);
             listaProdutos.Add(produto2);
             listaProdutos.Add(produto3);
 
             return Ok(listaProdutos);
-        }        
+
+        }
+
 
         // GET: api/Product/5
         public IHttpActionResult Get(int id)
         {
-            var produto1 = new Product();
-            produto1.ID = 1;
-            produto1.Name = "Placa de Rede";
-            produto1.Category = "Hardware";
-            produto1.Price = 120m;
-
-            var produto2 = new Product { ID = 2, Name = "Placa mãe", Category = "Hardware", Price = 480m };
-            var produto3 = new Product { ID = 3, Name = "Antivirus", Category = "Software", Price = 180m };
-
-            listaProdutos.Add(produto1);
-            listaProdutos.Add(produto2);
-            listaProdutos.Add(produto3);
-
-            var produto = listaProdutos.Find(x => x.ID == id);
+                       
+            //Busca o Id de um produto, e retorna o resultado (se o id foi encontrado)
+            var produto = listaProdutos.Find(x => x.Id == id);
             return Ok(produto);
+
         }
 
         // POST: api/Product
-        public void Post([FromBody]string value)
+        public IHttpActionResult Post([FromBody] Product produto)
         {
+            listaProdutos.Add(produto);
+            return Ok();
         }
 
         // PUT: api/Product/5
